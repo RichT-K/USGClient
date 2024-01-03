8e6478afd530e19ec9b1708ae9918fd86bb03981
richt-k <richard.kenny.1954@gmail.com>
2024-01-02 17:21:20 -0800

    build and publish client assets

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { onMount } from 'svelte';
+  const aPath = location.pathname.substr(1).toLowerCase().split("/");
+  const root = "/" +(aPath[0] || "");
+
+  let opened="";
+  let pathtext = root=="/"?"Try a different path":"... or not :)";
+  onMount(()=>{
+    console.log("IN SVELTE !!")
+    setTimeout(()=>{
+      opened="OPENED";
+    },1000);
+  })
+</script>
+<h1>Welcome To USGolfers</h1>
+<h2 class="{opened}">A Svelte Component Project
+</h2>
+<h3 class="{opened}">
+  <div>Root:{root}</div>
+
+  <div>Path:{aPath.join("/")}</div>
+
+  <div>
+    <a href="{root=="/"?"/a/different/path":"/"}">{pathtext}</a>
+  </div>
+</h3>
+<style>
+  h2{
+    width:0%;
+    white-space:pre;
+    overflow:hidden;
+    transition:all 1s ease;
+  }
+  h2.OPENED{
+    width:100%;
+    text-align: center;
+  }
+  h3{
+    max-height:0%;
+    white-space:pre;
+    overflow:hidden;
+    transition:all 4s ease;
+  }
+  h3.OPENED{
+    max-height:100%;
+    text-align: center;
+  }
+  a{
+    margin-top:2em;
+    text-decoration: underline;
+    color:white;
+  }
+</style>
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+	server: {
+        proxy: {
+        },
+    },	
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, 'src/main.js'),
+        name: 'USGolfers',
+        // the proper extensions will be added
+        fileName: 'assets',
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+          },
+        },
+      },
+    },
+  });

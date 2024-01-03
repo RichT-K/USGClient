a4172bd7ad16e4fc3745e6a40712e3762ae48de9
richt-k <richard.kenny.1954@gmail.com>
2024-01-02 22:21:25 -0800

    build and publish client assets

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,23 @@
+<svelte:options customElement="usg-app-component" />
 <script>
   import { onMount } from 'svelte';
-  const aPath = location.pathname.substr(1).toLowerCase().split("/");
-  const root = "/" +(aPath[0] || "");
-
-  let opened="";
-  let pathtext = root=="/"?"Try a different path":"... or not :)";
+  import {Routes,Router} from "/lib/Router"
   onMount(()=>{
     console.log("IN SVELTE !!")
-    setTimeout(()=>{
-      opened="OPENED";
-    },1000);
   })
 </script>
-<h1>Welcome To USGolfers</h1>
-<h2 class="{opened}">A Svelte Component Project
-</h2>
-<h3 class="{opened}">
-  <div>Root:{root}</div>
-
-  <div>Path:{aPath.join("/")}</div>
-
-  <div>
-    <a href="{root=="/"?"/a/different/path":"/"}">{pathtext}</a>
-  </div>
-</h3>
+<ul class="NavMenu">
+{#each Object.entries(Routes) as [path,Route], key}
+  <li><a href="{path}">{Route.text}</a></li>
+{/each}  
+</ul>
+<main use:Router />
 <style>
-  h2{
-    width:0%;
-    white-space:pre;
-    overflow:hidden;
-    transition:all 1s ease;
-  }
-  h2.OPENED{
-    width:100%;
-    text-align: center;
-  }
-  h3{
-    max-height:0%;
-    white-space:pre;
-    overflow:hidden;
-    transition:all 4s ease;
-  }
-  h3.OPENED{
-    max-height:100%;
-    text-align: center;
-  }
-  a{
-    margin-top:2em;
-    text-decoration: underline;
-    color:white;
+  .NavMenu{
+    display:grid;
+    gap:1em;
+    grid-auto-flow:column;
+    list-style: none;
+    justify-content: center;
   }
 </style>
